8625f39c9fdd2aa3dc4953d231104a678b49ecc9
pdrtuche <16190311-pdrtuche@users.noreply.replit.com>
2023-08-28 23:30:37 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <form action="teste.php" method="POST">
+        nome: <input type="text" name="nome" /><br>
+        idaade: <input type="text" name="idade"/><br>
+        <input type="submit" value="Enviar"/>
+    </form>
+        
+  </body>
+</html>
---
+++ teste.php
@@ -0,0 +1,6 @@
+<?php
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+echo "numero do usuario :".$nome;
+echo"<br>sua idade:".$idade;
+?>

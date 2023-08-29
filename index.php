5bab53faff69b7dd7cd05283ce0d4401d2760ea7
pdr-tuche <55400486+pdr-tuche@users.noreply.github.com>
2023-08-29 02:26:01 +0000

    colocado tag php no title

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<?php?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Test</title>
+    <title>
+        <?php
+            echo "PHP practice"
+        ?>
+    </title>
 </head>
 <body>
     <h1>IDADE</h1>

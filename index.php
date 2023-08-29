89f6edc1ec18e60a3ab57a8510588dd8400effa6
pdr-tuche <55400486+pdr-tuche@users.noreply.github.com>
2023-08-29 02:00:07 +0000

    colocado tag semantica para paragragos

--- index.php
+++ index.php
@@ -1,25 +1,46 @@
 <html>
   <head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Test</title>
   </head>
   <body>
+    <h1>IDADE</h1>
     <form action="teste.php" method="post">
-        nome: <input type="text" name="nome" /><br>
-        idaade: <input type="text" name="idade"/><br>
+        <p>nome</p>
+        <input type="text" name="nome" />
+        <br>
+        <p>idaade:</p>
+        <input type="text" name="idade"/>
+        <br>
         <input type="submit" value="Enviar"/>
     </form>
+      <hr>
 
+
+      
+<h1>COR PREFERIDA</h1>      
     <form action="teste2.php" method="get">
-        sua cor preferida: <input type="text" name="cor"/>
+        <p>sua cor preferida:</p>
+        <input type="text" name="cor"/>
         <input type="submit" value = "cor"/>
     </form>
+<hr>
 
-      <h1>nota aluno</h1>
+
+      
+      <h1>NOTA ALUNO</h1>
       <form action="media.php" method="get">
-        primeiranota: <input type="number" name="nota1"/>
-        segundanota: <input type="number" name="nota2"/>
-          terceiranota: <input type="number" name="nota3"/>
+        <p>primeiranota:</p>
+        <input type="number" name="nota1"/>
+        <p>segundanota:</p>
+        <input type="number" name="nota2"/>
+        <p>terceiranota:</p>
+        <input type="number" name="nota3"/>
         <input type="submit" value = "Enviar"/>
     </form>
+      <hr>
+      
   </body>
 </html>
+ 
